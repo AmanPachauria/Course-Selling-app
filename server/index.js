8162e5548abab9222ee3e41ed4d6fb51ba5748a7
aman <amanpachauria@gmail.com>
2023-12-18 20:19:47 +0530

    fix cors error

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(cors({
+    origin: 'https://yourdeployedsite.com'
+}));
+
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 
